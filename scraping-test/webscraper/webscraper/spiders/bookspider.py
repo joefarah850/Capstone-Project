c6722052ca3bce04a31a9d7de2e30cf3c810cce3
joefarah850 <joefarah850@gmail.com>
2023-09-10 22:24:30 +0200

    test

---
+++ scraping-test/webscraper/webscraper/spiders/bookspider.py
@@ -0,0 +1,46 @@
+import scrapy
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com"]
+
+    def parse(self, response):
+        books = response.css('article.product_pod')
+
+        for book in books:
+            relative_url = book.css('h3 a::attr(href)').get()
+
+            if 'catalogue/' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
+            else:
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+            yield response.follow(book_url, callback=self.parse_book_page)
+
+        next_page = response.css('li.next a::attr(href)').get()
+        
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
+
+    def parse_book_page(self, response):
+        table_rows = response.css('table tr')
+
+        yield {
+            'url': response.url,
+            'title': response.css('.product_main h1::text').get(),
+            'product_type': table_rows[1].css('td::text').get(),
+            'price_excl_tax': table_rows[2].css('td::text').get(),
+            'price_incl_tax': table_rows[3].css('td::text').get(),
+            'tax': table_rows[4].css('td::text').get(),
+            'availability': table_rows[5].css('td::text').get(),
+            'number_of_reviews': table_rows[6].css('td::text').get(),
+            'stars': response.css('.star-rating::attr(class)').get().replace('star-rating ', ''),
+            'category': response.xpath('/html/body/div/div/ul/li[3]/a/text()').get(),
+            'description': response.xpath('//*[@id="content_inner"]/article/p/text()').get(),
+            'price': response.css('p.price_color::text').get(),
+        }
