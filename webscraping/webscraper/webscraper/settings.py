bbb4c057065398de5a9d9b4943406251f35be9b2
dankvolta <maahdaoui@gmail.com>
2023-09-21 14:16:05 +0200

    new commits for reviewspider

--- webscraping/webscraper/webscraper/settings.py
+++ webscraping/webscraper/webscraper/settings.py
@@ -62,10 +62,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   "webscraper.pipelines.WebscraperPipeline": 300,
-   "webscraper.pipelines.SaveToMySQLPipeline": 400,
-}
+# ITEM_PIPELINES = {
+#    "webscraper.pipelines.WebscraperPipeline": 300,
+#    # "webscraper.pipelines.SaveToMySQLPipeline": 400,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
